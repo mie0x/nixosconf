e3dd62fbef7ce4b07ab9de78361511daa8f59f7e
mie <m.nazmi.mns@gmail.com>
2026-07-02 14:33:18 +0800

    update

--- cfg/pkgs.nix
+++ cfg/pkgs.nix
@@ -2,7 +2,7 @@
 
 { 
   #program enable default
-  #programs.firefox.enable = true;
+  programs.firefox.enable = true;
   programs.fish.enable = true;
   programs.dconf.enable = true;
   programs.gamemode.enable = true;
@@ -79,7 +79,7 @@
     nautilus
 
     #browser
-    firefox
+    #firefox
     
     #game
     prismlauncher
