058192094221107bb8c13df25e21fe637185a0d9
mie <m.nazmi.mns@gmail.com>
2026-08-14 16:16:30 +0800

    update

--- cfg/xdg.nix
+++ cfg/xdg.nix
@@ -5,18 +5,18 @@
     enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
-      #xdg-desktop-portal-gnome
+      xdg-desktop-portal-gnome
       xdg-desktop-portal-wlr
     ];
     xdgOpenUsePortal = true;
-    config.common.default = [ "wlr" "gtk3" ];
+    config.common.default = [ "wlr" "gtk3" "gnome" ];
   };
 
   # Required for the GNOME portal and file picking
   environment.systemPackages = with pkgs; [
     xdg-desktop-portal
     xdg-desktop-portal-gtk
-    #xdg-desktop-portal-gnome
+    xdg-desktop-portal-gnome
     #nautilus # Optional: Recommended fallback file manager for the gnome portal
   ];
   
